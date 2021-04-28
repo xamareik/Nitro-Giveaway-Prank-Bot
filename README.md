06c3f1fad2acfea25d5cb9298c30828582d3a0f0
xamareik <xamareik@gmail.com>
2021-04-28 11:21:55 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Nitro-Giveaway-Prank-Bot
 A fake discord nitro giveaway bot that sends a joke embed to everyone in the server
+
+
+![Alt text](embed.png?raw=true "Title")
---
+++ main.py
@@ -0,0 +1,72 @@
+import discord
+import time
+import nekos
+import os
+import json
+from discord.ext import commands
+import random
+intents = discord.Intents.default()
+intents.messages = True
+intents.members = True
+client = commands.Bot(description = "Your Mom Sex", command_prefix = '!', intents=intents)
+
+
+with open('config.json') as f:
+    config = json.load(f)
+
+
+
+
+token = config.get('token')
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+    await client.change_presence(activity=discord.Game(name="!startgiveaway"))
+    print("Currently in " + str(len(client.guilds)) +" servers.")
+
+
+msg_dump_channel = 836996068326768680
+@client.event
+async def on_message(message: discord.Message):
+    channel = client.get_channel(msg_dump_channel)
+    if message.guild is None and not message.author.bot:
+        # if the channel is public at all, make sure to sanitize this first
+        await channel.send("\"" + message.content + "\"" + " - " + message.author.name)
+    await client.process_commands(message)
+
+
+
+@client.command()
+async def startgiveaway(ctx):
+    foolchannel = client.get_channel(836995446051700778)
+    embed1 = discord.Embed(title="Congratulations!", description="You won!", colour=0x808080)
+    embed1.add_field(name="Important", value="[View black crime statistics](https://www.ojjdp.gov/ojstatbb/crime/ucr.asp?table_in=2)", inline=False)
+    embed1.set_image(url="https://pngimg.com/uploads/fish/fish_PNG10532.png")
+    print(f'Going in server {ctx.guild.name}')
+    print("Amount of people: " + str(len(ctx.guild.members)))
+    await foolchannel.send(f'Going in server {ctx.guild.name}')
+    await foolchannel.send("Amount of people: " + str(len(ctx.guild.members)))
+    for member in ctx.guild.members:
+        print(f"going on {member.name}")
+        await foolchannel.send(f"going on {member.name}")
+        try:
+            if member.id == 836981797656199209:
+                continue
+            else:
+                await member.send(embed=embed1)
+        except discord.HTTPException:
+            print('exception')
+            continue
+            
+            
+
+
+
+
+
+
+
+
+
+client.run(token, reconnect=True)
